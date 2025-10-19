04d53638b782b9d926655441410c6edacdd3ee60
Mechite <contact@mechite.com>
2025-10-19 17:13:39 +0100

    Push working tree for Mintlify documentation
    Signed-off-by: Mahied Maruf <contact@mechite.com>

--- docs/theme/index.js
+++ docs/theme/index.js
@@ -1,9 +1,11 @@
 const current = document.currentScript;
 if (!current || current.type === "module") throw new Error();
 
-fetch("https://midio.sirius.cam/theme/index.mjs").then(contents => {
-    const element = document.createElement("script");
-    element.type = "module";
-    element.innerHTML = contents.text();
-    current.insertAdjacentElement("afterend", element);
-})
+fetch("https://midio.sirius.cam/theme/index.mjs")
+    .then(response => response.text())
+    .then(contents => {
+        const element = document.createElement("script");
+        element.type = "module";
+        element.innerHTML = contents;
+        current.insertAdjacentElement("afterend", element);
+    });
